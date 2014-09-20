6cd5cdb85fb7f7b014e6be6d0ee8fcdfdfd3fb74
Jamar Vaughn <Jamarv@yahoo.com>
2014-09-20 11:52:33 -0400

    heroku update

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,31 @@
+class PostsController < ApplicationController
+  def index
+      @Content_first = "This is some sample text for my brand new ruby blog"
+      @Content_second = "This is more sample text for my brand new ruby blog"
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def show
+
+  end
+
+  def destroy
+
+  end  
+end
